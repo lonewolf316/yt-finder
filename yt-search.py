cc03c7afbce4320889d789857185dbf798d4d9f9
thewonelolf <3835177+lonewolf316@users.noreply.github.com>
2022-02-05 12:21:02 -0500

    added additional search method for better results

--- yt-search.py
+++ yt-search.py
@@ -1,3 +1,4 @@
+from distutils import extension
 import urllib.request, re, random, string
 from googleapiclient.discovery import build
 
@@ -8,14 +9,36 @@ def youtubeSetup(DEVELOPER_KEY):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
     return(youtube)
 
-# Collect random word from the configured word site
-# Get list of words from site, choose one randomly, and return single word as string
-def randomWord():
+#Get random word from MIT list
+def mitWord():
     wordSite = "https://www.mit.edu/~ecprice/wordlist.100000"
     wordString = urllib.request.urlopen(wordSite).read().decode()
     wordList = str(wordString).splitlines()
     finalWord = wordList[random.randint(0, len(wordList)-1)]
     return(finalWord)
+
+#Generate search phrase in the 'mov 1234' format
+def fileExt():
+    extensions = ["mov", "mp4", "mp3"]
+    randomExtent = extensions[random.randint(0, len(extensions)-1)]
+    randomNumber = str(random.randint(0, 9999)).zfill(4)
+    finalWord = randomExtent + " " + randomNumber
+    return(finalWord)
+
+#Determine where search query comes from, either manually specified or random generation
+def randomWord(searchType = "random"):
+    if searchType.lower() == "mit":
+        searchWord = mitWord()
+    elif searchType.lower() == "fileext":
+        searchWord = fileExt()
+    elif searchType.lower() == "random":
+        if random.randint(0,1) == 0:
+            searchWord = mitWord()
+        else:
+            searchWord = fileExt()
+    else:
+        searchWord = "invalidTerm"
+    return(searchWord)
 
 #performs search with basic youtube filter parameters and returns a list of video IDs 
 def youtubeSearch(youtube, searchTerm):
@@ -49,8 +72,12 @@ if __name__ == "__main__":
     import keys
     DEVELOPER_KEY = keys.YOUTUBE_KEY
     youtube = youtubeSetup(DEVELOPER_KEY) #create youtube api object
-    searchWord = randomWord() #generate a random word to search for - just returns a string
+    searchType = str(input("Search Type (mit, fileExt, random): "))
+    searchWord = randomWord(searchType=searchType) #generate a random word to search for - just returns a string
+    print("Searching term: " + str(searchWord))
     searchResults = youtubeSearch(youtube, searchWord) #pass youtube object and string into function to conduct a search. returns a list of <=50 results
     matchingResults = parseVideoData(youtube, searchResults) #results are parsed and checked for time and view count to match settings. returns list of video IDs
     linkList = idToLink(matchingResults) #video ID list is converted to list of links for ease of use.
-    print(linkList)
+
+    for link in linkList:
+        print(link)
